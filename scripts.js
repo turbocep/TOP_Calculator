af764b3d997c95757977db5b922a985c522f01b1
turbocep <162988238+turbocep@users.noreply.github.com>
2024-06-17 13:17:32 +0200

    create basic operation functions.

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//Functions:
+
+function add(operand1, operand2) {
+  return operand1 + operand2;
+}
+
+function subtract(operand1, operand2) {
+  return operand1 - operand2;
+}
+
+function multiply(operand1, operand2) {
+  return operand1 * operand2;
+}
+
+function divide(operand1, operand2) {
+  return operand1 / operand2;
+}
+
