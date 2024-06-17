18820aafd75b08f80be09f99d0921d18629fb3f4
turbocep <162988238+turbocep@users.noreply.github.com>
2024-06-17 13:46:41 +0200

    create and test updateDisplay();

--- scripts.js
+++ scripts.js
@@ -16,3 +16,29 @@ function divide(operand1, operand2) {
   return operand1 / operand2;
 }
 
+function operate(operand1, operator, operand2) {
+  //Create switch statement to perform correct operation.
+  switch (operator) {
+    case "+":
+      return add(operand1, operand2);
+    case "-":
+      return subtract(operand1, operand2);
+    case "*":
+      return multiply(operand1, operand2);
+    case "/":
+      return divide(operand1, operand2);
+  }
+}
+
+function updateDisplay(displayValue) {
+  document.querySelector(".screen p").textContent = displayValue;
+}
+
+//Variables:
+let operand1 = 5;
+let operator = "+";
+let operand2 = 3;
+let displayValue = "Hello";
+
+
+
